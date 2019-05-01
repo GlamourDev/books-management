d2bf1144e9e5e3d79edcb8183526f8fad97d7475
Henry Lõiv <33759098+GlamourDev@users.noreply.github.com>
2019-05-01 21:19:52 +0300

    Changed path and removed test

---
+++ src/frontend/build/precache-manifest.ae7f544dd0561416a086032934f3612d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f46420f22bb929676950",
+    "url": "/static/js/main.54c3d006.chunk.js"
+  },
+  {
+    "revision": "192947c4b8eab4cec02f",
+    "url": "/static/js/2.de98562c.chunk.js"
+  },
+  {
+    "revision": "f46420f22bb929676950",
+    "url": "/static/css/main.e9e98107.chunk.css"
+  },
+  {
+    "revision": "89e855d18a12c32847c99d4c5f94fb75",
+    "url": "/index.html"
+  }
+];
